c19f440d5864969f93fb07e394d8eb36b9fd3e05
GianniCampana <campanagianni97@gmail.com>
2021-06-06 16:42:58 +0200

    SNACK5

---
+++ snack5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>explode</title>
+</head>
+<body>
+
+
+
+
+<?php
+$stringa= "Lorem ipsum dolor sit amet consectetur adipisicing elit, Ducimus modi sit distinctio minus libero, reprehenderit possimus nisi placeat adipisci, odit maiores, iusto quod culpa accusamus, incidunt quaerat eaque, sint perspiciatis!"
+?>
+
+
+<h1><?php print_r(explode(',',$stringa)) ?></h1>
+    
+</body>
+</html>
